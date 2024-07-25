5ea3cbd449e254f574ab84f5cd7752060309eb67
Muriithi-ui <johnmuriithi339@gmail.com>
2024-07-25 11:47:39 +0300

    Added comments concerning the layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
-import SideNav from '@/app/ui/dashboard/sidenav';
+import SideNav from '@/app/ui/dashboard/sidenav'; //importing the <SideNav /> component into the layout. Any component imported into this this file will be part of the layout. 
 
-export default function Layout({ childern }: { childern: React.ReactNode}) {
+export default function Layout({ childern }: { childern: React.ReactNode}) {   //The <Layout /> component receives a `children` prop. This child can either be a page or another layout. In this case, the pages inside `/dashboard` will automatically be nested inside a `<Layout />`
    return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:-64">
